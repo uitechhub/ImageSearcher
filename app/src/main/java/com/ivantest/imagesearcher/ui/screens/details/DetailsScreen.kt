6d8db2b56c527a3a918ee93edb05ae652d609ded
Ivan Utsalo <uitechhub@gmail.com>
2025-01-15 19:31:51 -0500

    Detail Screen implementation first pass.
    Changes:
    1. Detail screen clean up.

--- app/src/main/java/com/ivantest/imagesearcher/ui/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/ivantest/imagesearcher/ui/screens/details/DetailsScreen.kt
@@ -1,9 +1,10 @@
 package com.ivantest.imagesearcher.ui.screens.details
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -12,10 +13,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewStateWithHTMLData
 import com.ivantest.imagesearcher.R
@@ -29,27 +30,26 @@ fun FlickrDetailView(
 ) {
     val webViewState = rememberWebViewStateWithHTMLData(data = image.description)
 
-    Column(modifier = modifier) {
-
-        IconButton(onClick = onBack, modifier = Modifier.size(44.dp)) {
-            Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                contentDescription = stringResource(R.string.back)
-            )
+    Column(modifier = modifier.padding(4.dp)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = onBack, modifier = Modifier.size(44.dp)) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.back)
+                )
+            }
+            Text(text = image.title, style = MaterialTheme.typography.titleLarge)
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
-        Image(painter = rememberAsyncImagePainter(image.imageUrl), contentDescription = null)
-        Text(text = image.title, style = MaterialTheme.typography.titleLarge)
-//        HtmlText(
-//            html = image.description
-//        )
-        WebView(state = webViewState)
-
         Text(text = "Author: ${image.author}", style = MaterialTheme.typography.bodyMedium)
         Text(
             text = "Published: ${image.published}",
             style = MaterialTheme.typography.bodyMedium
         )
+        WebView(state = webViewState)
+
     }
 }
